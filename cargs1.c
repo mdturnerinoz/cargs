8b2b389ed54e0d8b7507936c96e730a36a19a8fc
Marty Turner <developer@mdturner.com>
2013-01-31 09:46:45 +1100

    fixed bug in first printf - was ^s and should be %d

--- cargs1.c
+++ cargs1.c
@@ -3,16 +3,16 @@
 int main(int argc, char *argv[])
 {
    int i = 0;
-   printf("\ncargs1e args count=%s", argc);
+   printf("\ncargs1e args count: %d", argc);
 
    /* First argument is executable name only */
-   printf("\nexe name=%s", argv[0]);
+   printf("\nexe name: %s", argv[0]);
 
    for (i = 1; i < argc; i++)
    {
-      printf("\narg%d=%s", i, argv[i]);
+      printf("\narg%d: %s", i, argv[i]);
    }
 
-   printf("\n");
+   printf("\n\n");
    return 0;
 }
